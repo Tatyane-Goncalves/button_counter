40c1bc17014b0844e1bcc54cc8d1f97f9b536afb
tatyanepgoncalves <tatygoncalves023@gmail.com>
2025-07-21 19:49:06 -0300

    chore: Config inital with TS and Shadcn/UI

--- src/components/ButtonReset/ButtonReset.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ButtonReset.module.scss"
-
-export default function ButtonReset({ onReset }) {
-  return (
-    <button onClick={onReset} className={styles.btn_reset}>Resetar</button>
-  )
-}
